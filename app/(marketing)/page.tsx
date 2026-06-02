c39c86e72686b6c5eb9a54eaf852eac453cbd5cc
LiviuStoica73 <liviustoica73@gmail.com>
2026-06-02 12:35:10 +0000

    feat: remove InfoLanding section

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,8 +1,6 @@
-import { infos } from "@/config/landing";
 import BentoGrid from "@/components/sections/bentogrid";
 import Features from "@/components/sections/features";
 import HeroLanding from "@/components/sections/hero-landing";
-import InfoLanding from "@/components/sections/info-landing";
 import PreviewLanding from "@/components/sections/preview-landing";
 import Testimonials from "@/components/sections/testimonials";
 import { WorkflowSection } from "@/components/sections/workflow-section";
@@ -14,8 +12,6 @@ export default function IndexPage() {
       <PreviewLanding />
       <WorkflowSection />
       <BentoGrid />
-      <InfoLanding data={infos[0]} reverse={true} />
-      {/* <InfoLanding data={infos[1]} /> */}
       <Features />
       <Testimonials />
     </>
